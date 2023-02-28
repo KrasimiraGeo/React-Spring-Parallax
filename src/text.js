6b036dce90d58458e58e8b15996ac1c83a94f73a
Krasimira Geo <kraska.a.georgieva@gmail.com>
2023-02-28 14:27:53 +0200

    Initial commit

---
+++ src/text.js
@@ -0,0 +1,20 @@
+import './App.css';
+
+export function Text() {
+    return (
+        <div id="textblock">
+            <div id="textblock-container">
+                <h1 id="textblock-title">Hi, there!</h1>
+                <p id="textblock-content">
+                This is Krasimira Geo.<br/><br/>
+                And that is yet another practice project.<br></br>
+                This time the target of practice was the Parallax effect. <br></br>
+                Pretty cool, right! <br></br>
+                The project was built using React Spring and the built in Parallax components. 
+                </p>
+            </div>
+           
+        </div>
+    );
+}
+
